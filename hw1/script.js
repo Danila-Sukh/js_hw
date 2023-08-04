49915c13b3441040223e231482d579b7f8acc63a
Данила <suhorukovdanila@yandex.ru>
2023-08-04 17:12:03 +0300

    Add files via upload

---
+++ hw1/script.js
@@ -0,0 +1,13 @@
+/*Задание 1*/
+
+let temperature = 37;
+let fahrenheit = (9 / 5) * temperature + 32;
+console.log(`Температура в Цельсиях ${temperature}`);
+console.log(`Температура в Фаренгейтах ${fahrenheit}`);
+
+
+/*Задание 2*/
+
+let firstName = 'Aleksandra';
+let admin = firstName;
+console.log(admin);
